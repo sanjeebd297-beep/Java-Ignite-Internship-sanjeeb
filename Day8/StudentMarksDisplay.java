23ac7b2da52d879c5ffed6c7ad84e3284676392b
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-04 09:31:47 +0530

    Implement Student Marks Display functionality

--- Day8/StudentMarksDisplay.java
+++ Day8/StudentMarksDisplay.java
@@ -13,3 +13,23 @@ Student Marks:
 82
 55
   */
+package java_intership_day8;
+import java.util.Scanner;
+public class Student_Mark {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int marks[]=new int [10];
+		int i;
+		System.out.println("Enter the marks for the 10 students :");
+		for(i=0;i<10;i++) {
+			marks[i] = sc.nextInt();
+		}
+		System.out.println("Detailed marks :");
+		for(i=0;i<10;i++) {
+			System.out.printf("%d\n",marks[i]);
+		}
+		sc.close();
+	}
+}
