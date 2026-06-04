c77cc0de1bcda40c8a2679ee1604c54fb7b30e60
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-04 09:32:33 +0530

    Update Employee_Salary_Analyzer.java

--- Day8/Employee_Salary_Analyzer.java
+++ Day8/Employee_Salary_Analyzer.java
@@ -11,3 +11,25 @@ Hint
 
 Use a loop and a sum variable.
   */
+package java_intership_day8;
+import java.util.Scanner;
+public class Employee_Salary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int[] sal=new int [5];
+		int i,sum=0;
+		double avg;
+		
+		System.out.println("Enter the salary");
+		for(i=0;i<5;i++) {
+			sal[i]=sc.nextInt();
+			sum=sum+sal[i];
+		}
+		avg = sum/5.0;
+		System.out.printf("Total salary :%d\nAvg salary :%.2f\n",sum,avg);
+		sc.close();
+	}
+
+}
