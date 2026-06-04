0774d2b1bba22d448275d884d216c0fbf43123ab
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-04 09:35:36 +0530

    Implement consecutive increase checker for an array
    Added functionality to check if an array of numbers is in increasing order.

--- Day8/Consecutive_Increase_Checker.java
+++ Day8/Consecutive_Increase_Checker.java
@@ -20,3 +20,31 @@ Output:
 
 Array is NOT in increasing order
   */
+package JavaIgniteDay8;
+import java.util.Scanner;
+
+public class Consecutive_Increase_Checker {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc = new Scanner(System.in);
+			int[] arr = new int[5];
+			System.out.println("Enter 5 numbers:");
+			for(int i =0;i<arr.length;i++)
+			{
+				arr[i]= sc.nextInt();
+			}
+			
+			for(int i =0;i<arr.length-1;i++)
+			{
+				if(arr[i]>arr[i+1])
+				{
+					System.out.println("Array is not in increasing order!");
+					return;
+				}
+			}
+			System.out.println("Array is in increasing order!");
+			sc.close();
+		}
+
+	}
