61bcefc629cdf7739f08bcc00d60766273c2b3f3
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-04 09:14:20 +0530

    Update Attendance_Tracker.java

--- Day8/Attendance_Tracker.java
+++ Day8/Attendance_Tracker.java
@@ -14,3 +14,46 @@ Total Present Students
 Total Absent Students
 Attendance Percentage
 */
+package JavaIgniteDay8;
+import java.util.Scanner;
+
+public class Attendance_Tracker {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			Scanner sc = new Scanner(System.in);
+			int[] arr = new int[10];
+			System.out.println("Enter the attendance of 10 student:\n(1)-Present\t(0)-Absent");
+			for(int i =0;i<arr.length;i++)
+			{
+				arr[i]= sc.nextInt();
+			}
+
+			double count1=0,count2=0;
+			double present=0,absent=0;
+			for(int i=0;i<arr.length;i++)
+			{
+				if(arr[i]==1)
+				{
+					count1++;
+					present++;
+				}
+				else
+				{
+					count2++;
+					absent++;
+				}
+			}
+			double percentage1 = ((count1/10)*100);
+			double percentage = ((present/10)*100);
+
+			System.out.println("Total present students: "+count1);
+			System.out.println("Total absent students: "+count2);
+			System.out.println("Total present students: "+present);
+			System.out.println("Total absent students: "+absent);
+			System.out.println("Attendance percentage: "+percentage+"%");
+			sc.close()
+        }
+}
+
