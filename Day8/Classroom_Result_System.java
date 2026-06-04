3b43e8a742662cfb42393621d3fa7d4edf7c0f2e
sanjeeb 25bcsa50 <sanjeebd297@gmail.com>
2026-06-04 09:30:50 +0530

    Update Classroom_Result_System.java

--- Day8/Classroom_Result_System.java
+++ Day8/Classroom_Result_System.java
@@ -11,3 +11,26 @@ Output Example
 Passed Students: 7
 Failed Students: 3
 */
+package java_intership_day8;
+import java.util.Scanner;
+public class Class_mark {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int marks[]=new int [10];
+		int i,pass=0,fail=0;
+		System.out.println("Enter the marks for the 10 students :");
+		for(i=0;i<10;i++) {
+			marks[i] = sc.nextInt();
+		}
+		for(i=0;i<10;i++) {
+			if(marks[i] >= 35)
+				pass++;
+			else
+				fail++;
+		}
+		System.out.printf("Passed Students : %d\nFailed Student : %d\n",pass,fail);
+		sc.close();
+  }
+}
